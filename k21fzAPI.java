10cc7da04dd9c4423d7718ec5d6d37c54dbfd8a7
Swaraj Bandhu Prasad Kalwar <swarajbandhi0000@gmail.com>
2024-11-24 22:03:15 +0530

    done with some apiframework

--- k21fzAPI.java
+++ k21fzAPI.java
@@ -1,5 +1,14 @@
 package Apiframework;
+import static io.restassured.RestAssured.*;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 public class k21fzAPI {
+	
+	public static void main(String args[]) {
+		Response response = given().contentType(ContentType.JSON).queryParam("id","1").get("http://localhost:3000/demo");
+		response.prettyPrint();
+		
+	}
 
 }
